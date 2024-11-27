892d8385648a88c49066c918ae6a4760b2f53bd3
Dylan Ocampo <dylanomar@live.com.mx>
2024-11-27 11:37:36 -0600

    lights

--- src/components/Canvas3DScene.js
+++ src/components/Canvas3DScene.js
@@ -8,7 +8,8 @@ export const Canvas3DScene = () => {
     return (
         <>
             <Canvas camera={{position: [0, 0, 5]}} style={{position: "absolute"}} >
-            <directionalLight color="white" position={[0, 0, 2]} />
+            <directionalLight color="#84cab8" position={[5, 0, 2]} />
+            <directionalLight color="#582899" position={[-5, 0, 2]} />
             <ambientLight></ambientLight>
             <Avatar position={[0, -2.5, 2.5]} scale={2} />
             </Canvas>
