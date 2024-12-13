6c646cde2b52da7c2adf04012bffa152c61d2d06
RodrigoCuevasCa <108311383+RodrigoCuevasCa@users.noreply.github.com>
2024-12-13 10:19:04 -0600

    loginpage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,38 @@
 
 import '../styles/LoginPage.css';
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { ElementContextRoute } from "../context/RouteContext";
 export const LoginPage = () => {
     const {changeRoute} = useContext(ElementContextRoute);
+    const inputRefName = useRef(null);
+    const inputRefEmail = useRef(null);
+    const inputRefEdad = useRef(null);
     const onClickChangeRoute= () =>{
         changeRoute("Main")
     }
+    const onClickLogIn = async () => {
+        if(inputRefName.current.value.trim() != "" && inputRefEmail.current.value.trim() != "" && inputRefEdad.current.value.trim() != ""){
+            const response = fetch( "http://165.232.151.217/api/v1/signin",{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    name: inputRefName.current.value,
+                    email: inputRefEmail.current.value,
+                    age: inputRefEdad.current.value
+                })
+            })
+            .then(response => {
+                if(!response.ok){
+                    throw new Error("Error en el servidor");
+                }
+                return response.json();
+            }).then(data =>{
+                console.log(data);
+            })
+        }
+
     if(window.innerWidth <= 768){
         console.log("Mobil")
         return (
@@ -48,17 +74,17 @@ export const LoginPage = () => {
                 <div className='inputHolder' style={{display: "grid", justifyContent: "center"}}>
                     <div className= "inputName">
                         <h3 style={{color: "#5C1F99", marginBottom: "0", marginTop: "1.5vh"}}>Nombre</h3>
-                        <input placeholder="Nombre" type="text" className= "input_web"/>
+                        <input placeholder="Nombre" type="text" className= "input_web" ref={inputRefName}/>
                     </div>
                     <div className="inputEmail">
                         <h3 style={{color: "#5C1F99", marginBottom: "0", marginTop: "0", marginTop: "1.5vh"}}>Email</h3>
-                        <input placeholder='Email' type='email' className= "input_web"/>
+                        <input placeholder='Email' type='email' className= "input_web"ref={inputRefEmail}/>
                     </div>
                     <div className='inputAge'>
                         <h3 style={{color: "#5C1F99", marginBottom: "0", marginTop: "1.5vh"}}>Edad</h3>
-                        <input className= "input_web" placeholder='Edad' type="number" />
+                        <input className= "input_web" placeholder='Edad' type="number" ref={inputRefEdad} />
                     </div>
-                    <button onClick={onClickChangeRoute}  style ={{marginTop: "3vh", backgroundColor: "#5C1F99", color: "white", borderRadius: "25px", height: "6vh"}}>Continuar</button>
+                    <button onClick={onClickLogIn}  style ={{marginTop: "3vh", backgroundColor: "#5C1F99", color: "white", borderRadius: "25px", height: "6vh"}}>Continuar</button>
                 </div>
             </div>
             <h4 style={{fontSize: "12px", textAlign: "center", position: "fixed", bottom: "0"}}>Términos de servicio | Política de privacidad</h4>
@@ -66,4 +92,5 @@ export const LoginPage = () => {
         )
     }
 
+}
 }
